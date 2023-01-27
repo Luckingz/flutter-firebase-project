eb2031cafb310502c81b9d1e954522aae21e3154
LuckyME <luckyajidoku.com>
2023-01-27 20:00:12 +0100

    FutureBuilder, handled snapshot and ConnectionState

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -38,55 +39,67 @@ class _LoginState extends State<Login> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                controller: _email,
-                decoration: InputDecoration(
-                  alignLabelWithHint: true,
-                  hintText: 'Enter your E-mail Address',
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter your E-mail Address',
-                  icon: Icon(Icons.mail),
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextField(
-                controller: _password,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: InputDecoration(
-                  alignLabelWithHint: true,
-                  hintText: 'Enter your password',
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter your password',
-                  icon: Icon(Icons.password),
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              ElevatedButton(onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
-                final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-              }, child: Text('Login')),
-              Row(
-                children: [
-                  Text('New User?'),
-                  TextButton(onPressed: null, child: Text('Create an account'))
-                ],
-              )
-            ],
-          )
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          //options: DefaultFirebaseOptions.currentPlatform,
         ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Container(
+                child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _email,
+                          decoration: InputDecoration(
+                            alignLabelWithHint: true,
+                            hintText: 'Enter your E-mail Address',
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter your E-mail Address',
+                            icon: Icon(Icons.mail),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        TextField(
+                          controller: _password,
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          decoration: InputDecoration(
+                            alignLabelWithHint: true,
+                            hintText: 'Enter your password',
+                            border: OutlineInputBorder(),
+                            labelText: 'Enter your password',
+                            icon: Icon(Icons.password),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        ElevatedButton(onPressed: () async {
+                          final email = _email.text;
+                          final password = _password.text;
+                          final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+                        }, child: Text('Login')),
+                        Row(
+                          children: [
+                            Text('New User?'),
+                            TextButton(onPressed: null, child: Text('Create an account'))
+                          ],
+                        )
+                      ],
+                    )
+                ),
+              );
+            default:
+              return Text('loading...');
+          }
+        }
       )
     );
   }
